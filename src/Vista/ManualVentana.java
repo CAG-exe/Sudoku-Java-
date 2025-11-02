aa58281a5da62deaedd197d4b5d1b96316884488
rodrigoAr2005 <rodrigoaranda609@gmail.com>
2025-11-02 13:13:25 -0300

    modificación del manual

--- src/Vista/ManualVentana.java
+++ src/Vista/ManualVentana.java
@@ -15,7 +15,7 @@ public class ManualVentana extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         JTextArea manualTexto = new JTextArea();
-        manualTexto.setFont(new Font("Monospaced", Font.PLAIN, 15));
+        manualTexto.setFont(new Font("Arial", Font.PLAIN, 24));
         manualTexto.setEditable(false);
         
         String manualContenido = 
@@ -23,23 +23,21 @@ public class ManualVentana extends JFrame {
         "1. MENÚ PRINCIPAL" + "\n" +
         "\n" +
         "Al iniciar, puede elegir cómo comenzar el juego:" + "\n" + "\n" +
-        "- Generar: Pide ingresar una cantidad de valores prefijados." + "\n" +
+        "- Generar Manualmente: Puedes ingresar valores prefijados" + "\n" +
+        "de forma manual" + "\n" + "\n" +
+        "- Generar Aleatoriamente: Pide ingresar una cantidad de valores prefijados." + "\n" + 
         "El rango válido es entre 15 y 40." + "\n" + "\n" +
-        "- Generar Aleatoriamente: Inicia un juego con valores prefijados" + "\n" + 
-        "al azar (entre 15 y 40)." + "\n" + "\n" +
         "2. INTERFAZ DEL JUEGO" + "\n" +"\n" +
-        "Una vez en el tablero, tiene los siguientes controles:" + "\n" + "\n" +
-        "- Comprobar solución: Verifica si su tablero tiene" + "\n" +
-        "al menos una solución posible." + "\n" + "\n" +
-        "- Mostrar solución: Rellena el tablero con la solución encontrada." + "\n" + "\n" +
-        "- Generar Sudoku: Inicia un nuevo juego pidiendo una cantidad" + "\n" + 
-        "de valores prefijados." + "\n" + "\n" +
+        "Una vez en el tablero, tienes el siguiente botón:" + "\n" + "\n" +
+        "- Buscar soluciones: Busca todas las soluciones posibles del sudoku y"
+        + "\n" + "lo mostrará; caso contrario, no se observará ninguna solucion." +
+        "\n" + "\n" +
         "3. INTERACCIÓN CON EL TABLERO" + "\n" + "\n" +
-        "- Casillas Prefijadas: Son los números iniciales" + "\n" +
+        "- Casillas Prefijadas: Son los números iniciales." + "\n" +
         "No pueden ser editados." + "\n" + "\n" +
         "- Si un número se repite en fila,columna o cuadrante, no se lo tomará." + 
         "\n" + "\n" +
-        "- Al seleccionar una celda, su fila, columna y cuadrante" + "\n" + 
+        "- Al seleccionar una celda su fila, columna y cuadrante" + "\n" + 
         "son resaltadas para facilitar la visualización de restricciones." + "\n" +
         "\n";
         
